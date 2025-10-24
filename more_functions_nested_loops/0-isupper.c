441cdea1de421e8aacd5bc1c3a4e6587ce3aafc8
root <yara241020@gmail.comgit config --global user.name your0T0>
2025-10-24 18:58:53 +0000

    Project: more_functions_nested_loops - task 0 (_isupper) with README and headers

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
-* _isupper - checks for uppercase character
-* @c: character to check (ASCII code)
-*
-* Return: 1 if c is uppercase, 0 otherwise
-*/
+ * _isupper - checks for uppercase character
+ * @c: character to check (ASCII code)
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-return (0);
+	if (c >= 'A' && c <= 'Z')
+           return (1);
+	return (0);
 }
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,3 +5,4 @@ int _putchar(char c);
 int _isupper(int c);
 
 #endif /* MAIN_H */
+
