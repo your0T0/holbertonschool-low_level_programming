eeed78204b65151e532ab185ce7cce6937902cec
your0T0 <yara241020@gmail.com>
2025-10-26 16:25:37 +0000

    Fix print_line function

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -17,7 +17,9 @@ void print_line(int n)
     else
     {
         for (i = 0; i < n; i++)
-            putchar('');
+        {
+           _putchar('_');
+         }
         _putchar('\n');
     }
 }
