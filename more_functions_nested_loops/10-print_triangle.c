64cec5d7a4ee3193adba4a0ad5f04a90232f0de9
your0T0 <yara241020@gmail.com>
2025-10-26 19:10:25 +0000

    Fix Betty style (tabs, EOL, trailing spaces) for tasks 3-10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,29 +1,27 @@
 #include "main.h"
-
 /**
- * print_triangle - prints a triangle followed by a new line
- * @size: the size of the triangle
- *
- * Return: void
- */
+* print_triangle - prints a triangle followed by a new line
+* @size: the size of the triangle
+*
+* Return: void
+*/
 void print_triangle(int size)
 {
-    int row, space, hash;
-
-    if (size <= 0)
-    {
-	_putchar('\n');
-    }
-    else
-    {
-	for (row = 1; row <= size; row++)
-	{
-	    for (space = 0; space < size - row; space++)
-		_putchar(' ');
-	    for (hash = 0; hash < row; hash++)
-		_putchar('#');
-	    _putchar('\n');
-	}
-    }
+int row, space, hash;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (space = 0; space < size - row; space++)
+_putchar(' ');
+for (hash = 0; hash < row; hash++)
+_putchar('#');
+_putchar('\n');
+}
+}
 }
 
--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -10,5 +10,5 @@ int i;
 for (i = 0; i <= 9; i++)
 _putchar(i + '0');
 _putchar('\n');
-} 
+}
 
