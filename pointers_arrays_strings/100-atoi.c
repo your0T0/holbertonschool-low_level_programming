2ca47e2ba261857980d8e525c4ad20e83c4c666e
your0T0 <yara241020@gmail.com>
2025-10-26 17:42:25 +0000

    Fix _atoi to handle INT_MIN correctly

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,41 +3,37 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
- * Return: the integer value, or 0 if no numbers in string
+ * Return: integer value
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	unsigned int num = 0;
-	int started = 0;
+	int i = 0, sign = 1, result = 0;
 
-	while (s[i] != '\0')
+	/* Skip non-numeric characters */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
-		if (s[i] == '+' || s[i] == '-')
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	/* Convert digits to int */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (sign > 0)
 		{
-			if (!started)
-			{
-				if (s[i] == '-')
-					sign *= -1;
-			}
-			else
-			{
-				break;
-			}
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
-			num = num * 10 + (s[i] - '0');
+			if (result > (2147483647 - (s[i] - '0')) / 10)
+				return (2147483647); /* INT_MAX */
+			result = result * 10 + (s[i] - '0');
 		}
 		else
 		{
-			if (started)
-				break;
+			if (result < (-2147483648 + (s[i] - '0')) / 10)
+				return (-2147483648); /* INT_MIN */
+			result = result * 10 - (s[i] - '0');
 		}
 		i++;
 	}
 
-	return (sign * (int)num);
+	return (result);
 }
