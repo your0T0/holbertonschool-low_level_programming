71a2c798b63b8977daec786feb6dc33ce4f79dc9
your0T0 <your0T0@github.com>
2025-10-31 18:46:05 +0000

    fixed 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
- * _memcpy - copies memory area
- * @dest: destination buffer
- * @src: source buffer
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
- */
+* _memcpy - copies memory area
+* @dest: destination buffer
+* @src: source buffer
+* @n: number of bytes to copy
+*
+* Return: pointer to dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-
-    for (i = 0; i < n; i++)
-        dest[i] = src[i];
-
-    return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+dest[i] = src[i];
+return (dest);
 }
