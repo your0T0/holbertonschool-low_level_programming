782825169531d445f0e2fa08683d1a14d0bf7f5f
your0T0 <your0T0@github.com>
2025-10-31 16:39:25 +0000

    add 0-memset task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,23 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
-
-/* 0 */
-char *_strcat(char *dest, char *src);
-/* 1 */
-char *_strncat(char *dest, char *src, int n);
-/* 2 */
-char *_strncpy(char *dest, char *src, int n);
-/* 3 */
-int _strcmp(char *s1, char *s2);
-/* 4 */
-void reverse_array(int *a, int n);
-/* 5 */
-char *string_toupper(char *);
-/* 6 */
-char *cap_string(char *);
-/* 7 */
-char *leet(char *);
-
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif /* MAIN_H */
