4d295bc50a2843e4c185cce1637bb8a782af0691
your0T0 <your0T0@github.com>
2025-11-01 10:46:10 +0000

    mmmm

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - prints the program name
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0
- */
-int main(int argc _attribute_((unused)), char *argv[])
+* main - prints the program name
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - prints the number of arguments passed
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0
- */
-int main(int argc, char *argv[] _attribute_((unused)))
+* main - prints the number of arguments passed
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	return (0);
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
 }
