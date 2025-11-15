e715286e0d66f2f2aa69b1018b799683ddbb705a
your0T0 <yara241020@gmail.com>
2025-11-15 16:47:50 +0000

    Fix print_all: removed macros and cleaned for betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,28 +1,19 @@
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-#include "variadic_functions.h"
 
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
  *
- * Types:
- *  c: char
- *  i: integer
- *  f: float
- *  s: char * (if NULL, print (nil))
- *
- * Return: Nothing.
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0;
-	char *s;
-	char *sep = "";
-	int n;
-	double d;
-	char c;
+	int i = 0;
+	char *str;
+	char *separator = "";
 
 	va_start(ap, format);
 
@@ -31,30 +22,25 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				c = (char)va_arg(ap, int);
-				printf("%s%c", sep, c);
-				sep = ", ";
+				printf("%s%c", separator, va_arg(ap, int));
 				break;
 			case 'i':
-				n = va_arg(ap, int);
-				printf("%s%d", sep, n);
-				sep = ", ";
+				printf("%s%d", separator, va_arg(ap, int));
 				break;
 			case 'f':
-				d = va_arg(ap, double);
-				printf("%s%f", sep, d);
-				sep = ", ";
+				printf("%s%f", separator, va_arg(ap, double));
 				break;
 			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
-					s = "(nil)";
-				printf("%s%s", sep, s);
-				sep = ", ";
+				str = va_arg(ap, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", separator, str);
 				break;
 			default:
-				break;
+				i++;
+				continue;
 		}
+		separator = ", ";
 		i++;
 	}
 
